3ae7f69c0c8c5d745a8011c1ec45f4f2579a3b37
Skitt2 <provaid@yandex.ru>
2024-06-02 11:16:52 +0300

    Время готовки задано в виде ключ-значение

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,23 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    let time = ["Soft": 5, "Medium": 7, "Hard": 12]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         let hardness = sender.currentTitle!
         
-        if hardness == "Soft" {
-            print(softTime)
-        } else {
-            if hardness == "Medium" {
-                print(mediumTime)
-            } else {
-                print (hardTime)
-            }
-        }
+        print(time[hardness]!)
         
     }
     
